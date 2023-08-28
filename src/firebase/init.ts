35883485000759566509729fe443323112c10a28
AlexKharenko <alexey16052002@gmail.com>
2023-08-28 16:42:07 +0300

    bug: fixed export bug

--- src/firebase/init.ts
+++ src/firebase/init.ts
@@ -1,15 +1,7 @@
-import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
-const auth = getAuth();
 const firestore = getFirestore();
 const storage = getStorage();
 
-const exportOptions = {
-    firebaseAuth: auth,
-    firestore,
-    firestorage: storage,
-};
-
-export default exportOptions;
+export { firestore, storage as firestorage };
