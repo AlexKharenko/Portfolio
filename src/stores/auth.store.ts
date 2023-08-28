51ecbb43c68daa5398a5b607b51d16199e9a904d
AlexKharenko <alexey16052002@gmail.com>
2023-08-28 16:41:45 +0300

    feat: store for auth

--- src/stores/auth.store.ts
+++ src/stores/auth.store.ts
@@ -1,11 +1,39 @@
 import { ref } from "vue";
 import { defineStore } from "pinia";
+import { getAuth, signInWithEmailAndPassword, signOut as SignOutFirebase } from "firebase/auth";
+
+type Response = { success: true } | { success: false; message: string };
 
 export const useAuthStore = defineStore("auth", () => {
+    const auth = getAuth();
     const isAuthenticated = ref(false);
     const setAuthenticationStatus = (state: boolean) => {
         isAuthenticated.value = state;
     };
 
-    return { isAuthenticated, setAuthenticationStatus };
+    const signIn = async (email: string, password: string): Promise<Response> => {
+        try {
+            const user = await signInWithEmailAndPassword(auth, email, password);
+            setAuthenticationStatus(true);
+            return { success: true };
+        } catch (error: any) {
+            setAuthenticationStatus(false);
+            // if (error instanceof FirebaseError) {
+            //     console.log(error.code);
+            // }
+            return { success: false, message: error.message };
+        }
+    };
+
+    const signOut = async (): Promise<{ success: boolean }> => {
+        try {
+            await SignOutFirebase(auth);
+            setAuthenticationStatus(false);
+            return { success: true };
+        } catch {
+            return { success: false };
+        }
+    };
+
+    return { isAuthenticated, setAuthenticationStatus, signIn, signOut };
 });
