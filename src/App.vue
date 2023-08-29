e86543341d6f415d14b7be432aa83ecadc6353b8
AlexKharenko <alexey16052002@gmail.com>
2023-08-29 21:08:02 +0300

    Basic app structure

--- src/App.vue
+++ src/App.vue
@@ -1,82 +1,33 @@
-<script setup lang="ts">
-import { RouterLink, RouterView } from "vue-router";
-</script>
-
 <template>
-    <header>
-        <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-        <div class="wrapper">
-            <nav>
-                <RouterLink to="/">Home</RouterLink>
-                <RouterLink to="/about">About</RouterLink>
-            </nav>
-        </div>
-    </header>
-
-    <RouterView />
+    <NavBar v-if="showNavBar"></NavBar>
+    <main class="main-wrapper">
+        <RouterView />
+    </main>
 </template>
 
-<style scoped>
-header {
-    line-height: 1.5;
-    max-height: 100vh;
-}
+<script setup lang="ts">
+import { RouterView, useRoute } from "vue-router";
+import NavBar from "./components/NavBar.vue";
+import { computed } from "vue";
 
-.logo {
-    display: block;
-    margin: 0 auto 2rem;
-}
+const router = useRoute();
+const showNavBar = computed(() => {
+    if (router.path.startsWith("/admin")) {
+        return false;
+    }
+    return true;
+});
+</script>
 
-nav {
+<style lang="scss" scoped>
+.main-wrapper {
+    display: flex;
+    flex-direction: column;
+    flex-grow: 1;
+    // max-width: calc(1280px + 1rem * 2);
+    // padding: 0 1rem;
     width: 100%;
-    font-size: 12px;
-    text-align: center;
-    margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-    color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-    background-color: transparent;
-}
-
-nav a {
-    display: inline-block;
-    padding: 0 1rem;
-    border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-    border: 0;
-}
-
-@media (min-width: 1024px) {
-    header {
-        display: flex;
-        place-items: center;
-        padding-right: calc(var(--section-gap) / 2);
-    }
-
-    .logo {
-        margin: 0 2rem 0 0;
-    }
-
-    header .wrapper {
-        display: flex;
-        place-items: flex-start;
-        flex-wrap: wrap;
-    }
-
-    nav {
-        text-align: left;
-        margin-left: -1rem;
-        font-size: 1rem;
-
-        padding: 1rem 0;
-        margin-top: 1rem;
-    }
+    // margin: 0 auto;
+    position: relative;
 }
 </style>
