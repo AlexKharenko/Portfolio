ccf58695c9479b589e7cb94118e1069aea7e6d6e
AlexKharenko <alexey16052002@gmail.com>
2023-09-03 00:40:16 +0300

    feat: add svg icons

--- src/components/icons/AdminPanel/PlusIcon.vue
+++ src/components/icons/AdminPanel/PlusIcon.vue
@@ -1,7 +1,10 @@
 <template>
     <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-
-        <path d="M6 12H18M12 6V18" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-        </path>
+        <path
+            d="M6 12H18M12 6V18"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"></path>
     </svg>
 </template>
