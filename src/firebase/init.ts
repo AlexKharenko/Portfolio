d6c7ad9650660f291ebf61c4e40af83f328fb73c
AlexKharenko <alexey16052002@gmail.com>
2023-09-13 20:04:23 +0300

    feat: delete firebase init file

--- src/firebase/init.ts
+++
@@ -1,7 +0,0 @@
-import { getFirestore } from "firebase/firestore";
-import { getStorage } from "firebase/storage";
-
-const firestore = getFirestore();
-const storage = getStorage();
-
-export { firestore, storage as firestorage };
