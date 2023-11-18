71de6e330b7c985811fdef99555b03577a16fa02
AlexKharenko <alexey16052002@gmail.com>
2023-11-18 21:10:22 +0200

    feat: updated empty zod rule to new

--- src/schemas/createEditTag.zod-schema.ts
+++ src/schemas/createEditTag.zod-schema.ts
@@ -3,7 +3,6 @@ import * as zod from "zod";
 const validationSchema = zod.object({
     tagName: zod
         .string()
-        .nonempty("This field is required")
         .min(2, { message: "Tag length must be between 2 and 10 characters" })
         .max(10, { message: "Tag length must be between 2 and 10 characters" }),
 });
--- src/schemas/signin.zod-schema.ts
+++ src/schemas/signin.zod-schema.ts
@@ -3,11 +3,10 @@ import * as zod from "zod";
 const validationSchema = zod.object({
     email: zod
         .string()
-        .nonempty("This field is required")
+        .min(1, "This field is required")
         .email({ message: "Must be a valid email" }),
     password: zod
         .string()
-        .nonempty("This field is required")
         .min(8, { message: "Password length must be between 8 and 16 characters" })
         .max(16, { message: "Password length must be between 8 and 16 characters" }),
 });
